e996aa715266a4b244302ccfebcf3ff592582719
iTz-samuel11 <samuelandresfaraco@gmail.com>
2022-05-29 02:03:32 +0000

    ajuste en funcionadilad

--- src/js/component/Semaforo.js
+++ src/js/component/Semaforo.js
@@ -4,54 +4,34 @@ export const Semaforo = () => {
 	const [red, setRed] = useState("#8B0000 ");
 	const [green, setGreen] = useState("#008C00");
 	const [yellow, setYellow] = useState("#9B870C");
-	const [yellowShadow, setYellowShadow] = useState("");
-	const [greenShadow, setGreenShadow] = useState("");
-	const [redShadow, setRedShadow] = useState("");
+	const [shadow, setShadow] = useState("");
 	return (
 		<div className="container">
-			<div
-				className="bg-dark position-absolute top-50 start-50 translate-middle"
-				style={{
-					width: "180px",
-					height: "420px",
-					borderRadius: "40px",
-				}}>
+			<div className="bg-dark position-absolute top-50 start-50 translate-middle">
 				<div
-					className="rounded-circle position-absolute top-0 start-50 translate-middle-x "
+					className={`rounded-circle position-absolute top-0 start-50 translate-middle-x ${
+						shadow === "red" ? "shadow-1" : ""
+					}`}
 					style={{
-						width: "130px",
-						height: "130px",
 						background: red,
-						boxShadow: redShadow,
 					}}
-					onClick={() => {
-						setRed("red");
-						setRedShadow("0px 0px 20px 10px #0ff");
-					}}></div>
+					onClick={() => setShadow("red")}></div>
 				<div
-					className="rounded-circle position-absolute top-50 start-50 translate-middle "
+					className={`${
+						shadow === "yellow" ? "shadow-1" : ""
+					} rounded-circle position-absolute top-50 start-50 translate-middle`}
 					style={{
-						width: "130px",
-						height: "130px",
 						background: yellow,
-						boxShadow: yellowShadow,
 					}}
-					onClick={() => {
-						setYellow("yellow");
-						setYellowShadow("0px 0px 20px 10px #0ff");
-					}}></div>
+					onClick={() => setShadow("yellow")}></div>
 				<div
-					className="rounded-circle position-absolute bottom-0 start-50 translate-middle-x bg-success"
+					className={`rounded-circle position-absolute bottom-0 start-50 translate-middle-x bg-success ${
+						shadow === "green" ? "shadow-1" : ""
+					}`}
 					style={{
-						width: "130px",
-						height: "130px",
 						background: green,
-						boxShadow: greenShadow,
 					}}
-					onClick={() => {
-						setGreen("green");
-						setGreenShadow("0px 0px 20px 10px #0ff");
-					}}></div>
+					onClick={() => setShadow("green")}></div>
 			</div>
 		</div>
 	);
